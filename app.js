5b851d65c034640836b7b1f4c56bfd4675579c39
Samyak Jain <samyakjain971@gmail.com>
2021-09-04 00:49:34 +0530

    binding&computed

--- app.js
+++ app.js
@@ -2,27 +2,26 @@ const app = Vue.createApp({
     // data, functions 
     data(){
         return{
+            url: 'https://www.thenetninja.co.uk',
             showBooks : true,
-            title: "The Final Empire",
-            author: 'Brandon Stark',
-            age: 45,
-            x : 0,
-            y : 0
+            books: [
+              { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+              { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+              { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true },
+            ],
         }
     },
     methods: {
         toggleShowBooks(){
             this.showBooks = !this.showBooks
         },
-        handleEvent(e, data){
-            console.log(e, e.type)
-            if(data){
-                console.log(data)
-            }
+        toggleFav(book){
+            book.isFav = !book.isFav
         },
-        handleMouseMove(e){
-            this.x = e.offsetX,
-            this.y = e.offsetY
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter( (book) => book.isFav)
         }
     }
 })
