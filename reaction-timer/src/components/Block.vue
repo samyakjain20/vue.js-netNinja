2f52539c7240a97f9d1c168db418f5098c2e58cd
Samyak Jain <samyakjain971@gmail.com>
2021-09-04 21:50:05 +0530

    Reaction Timer Project

---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+      Click Me
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    
+    data() {
+        return{
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    mounted() {
+        console.log('Component Mounted'),
+        setTimeout(() => {
+            this.startTimer(),
+            this.showBlock = true
+        }, this.delay)
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime +=10
+            }, 10)
+        },
+        stopTimer() {
+            clearInterval(this.timer),
+            console.log('Stopped timer', this.reactionTime),
+            this.$emit('end', this.reactionTime)
+        }
+    }
+}
+</script>
+
+<style>
+  .block {
+    width: 400px;
+    border-radius: 20px;
+    background:  #0faf87;;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+  }
+</style>
